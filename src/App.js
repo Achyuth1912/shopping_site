06975e67bfa509a1cf3cc7abf86b73001f723378
Achyuth <smachyuth12@gmail.com>
2019-05-05 16:37:56 +0530

    added support for css module and ejected the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    /*<div className="App">*/
+    <div className={style.App}>
+      
+      <header className={style.AppHeader}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
