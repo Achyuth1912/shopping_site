d9ad9e659b902a081b52b152698f3e8db15b795e
Achyuth <smachyuth12@gmail.com>
2019-05-05 16:57:02 +0530

    Adding a static navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import style from './App.css';
+import styles from './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    /*<div className="App">*/
-    <div className={style.App}>
-      
-      <header className={style.AppHeader}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.app}>
+        <Navbar/>
     </div>
   );
 }
